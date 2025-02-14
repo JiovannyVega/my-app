58c9689501586fcf8fc3791ac0769f2ed3e94ad8
JiovannyVega <erickjiovannygamezvega@hotmail.com>
2025-02-14 02:32:28 -0600

    Agregar pantallas y estilos para Home, Profile y Settings; configurar navegación con tabs

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/home/HomeScreen';
+import SettingsScreen from '../screens/settings/SettingsScreen';
+import ProfileScreen from '../screens/profile/ProfileScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function AppNavigator() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen name="Settings" component={SettingsScreen} />
+                <Tab.Screen name="Profile" component={ProfileScreen} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
