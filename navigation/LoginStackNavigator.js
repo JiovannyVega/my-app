aa7fc47c5f659d94c07f61bcf52810152ce19cfc
JiovannyVega <erickjiovannygamezvega@hotmail.com>
2025-02-24 08:37:09 -0600

    Add Datos screen to Login stack navigator

--- navigation/LoginStackNavigator.js
+++ navigation/LoginStackNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../screens/login/LoginScreen';
+import DatosScreen from '../screens/datos/DatosScreen';
 
 const Stack = createStackNavigator();
 
@@ -8,6 +9,7 @@ export default function HomeStackNavigator() {
     return (
         <Stack.Navigator>
             <Stack.Screen name="LoginScreen" component={LoginScreen} />
+            <Stack.Screen name="DatosScreen" component={DatosScreen} />
         </Stack.Navigator>
     );
 }
