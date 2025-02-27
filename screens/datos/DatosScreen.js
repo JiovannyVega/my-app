ce8c8a2342412dd696caf478aa0e1b5c069d56d2
feralcojam <feral.cojam@gmail.com>
2025-02-27 09:13:20 -0600

    Terminar de agregar datos faltantes a la pantalla; se agregó base para comenzar a diseñar pantalla Datos

--- screens/datos/DatosScreen.js
+++ screens/datos/DatosScreen.js
@@ -1,53 +1,19 @@
 import React from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, StatusBar } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import styles from './DatosScreen.styles.js';
 
 export default function HomeScreen() {
     const navigation = useNavigation();
-    const [curp, onChangeCURP] = React.useState('');
-    const [email, onChangeEmail] = React.useState('');
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Inicia sesión</Text>
-            <Text style={styles.subtitle}>Ingresa tus datos a continuación</Text>
-            <View style={styles.form}>
-                <TextInput
-                    style={styles.formInput}
-                    onChangeText={onChangeCURP}
-                    value={curp}
-                    placeholder='Ingresa tu CURP'
-                    placeholderTextColor='#C5C5C5'
-                    autoCapitalize='characters'
-                />
-                <TouchableOpacity
-                    onPress={() => {
-                        // Ir a página de consulta tu CURP
-                    }}
-                >
-                    <View>
-                        <Text style={styles.textLink}>Consulta tu CURP</Text>
-                    </View>
-                </TouchableOpacity>
-                <TextInput
-                    style={styles.formInput}
-                    onChangeText={onChangeEmail}
-                    value={email}
-                    placeholder='Ingresa tu correo electrónico'
-                    placeholderTextColor='#C5C5C5'
-                    autoCapitalize='none'
-                    keyboardType='email-address'
-                />
-                <TouchableOpacity
-                    onPress={() => {
-                        // Ingresar
-                    }}
-                >
-                    <View style={styles.btn}>
-                        <Text style={styles.loginBtn}>Ingresar</Text>
-                    </View>
-                </TouchableOpacity>
+            <StatusBar backgroundColor="#184666" barStyle="light-content" />
+            <View style={styles.dataView}>
+                
+            </View>
+            <View style={styles.bodyView}>
+                
             </View>
         </View>
     );
